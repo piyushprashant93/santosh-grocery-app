6fed81704117dbeb86a566fc33b93d3d81477fcc
piyushprashant93 <piyushprashant93@gmail.com>
2026-09-25 18:49:16 +0530

    feat: add Footer to SignIn page

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,11 +1,13 @@
 import Header from '../layout/Header'
 import ChooseExperience from '../components/signinpage/ChooseExperience'
+import Footer from '../layout/Footer'
 
 export const SignIn = () => {
   return (
     <>
       <Header />
       <ChooseExperience />
+      <Footer />
     </>
   )
 }
